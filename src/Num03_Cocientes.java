47a076531b3154e67d136f0616c478a22a492e54
Rafael Eulalio Villaneda de la torre <Rafael.rafaeua2401@outlook.com>
2020-10-07 18:16:51 -0500

    El punto 3 se termino correctamente

--- src/Num03_Cocientes.java
+++ src/Num03_Cocientes.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 class MostrarCocientesEnteros{
-	public static void mostrarCocientes(int n,int n2,int x) {
-		if(n>1 && n2>1) {
-			
+	public static void mostrarCocientes(int n,int n2,int num) {
+		double cociente=0.0;
+		if(num>=n) {
+			cociente=n2/num;
+			if(n2%num==0) {
+				System.out.println(cociente);
+				mostrarCocientes(n, n2, num-1);
+			}else {
+				mostrarCocientes(n, n2, num-1);
+			}
+		}else if(num==n) {
+			System.out.println(n2);
 		}
 	}
 }
@@ -11,11 +20,12 @@ public class Num03_Cocientes {
 
 	public static void main(String[] args) {
 		Scanner entrada=new Scanner(System.in);
-		System.out.println("Imgrese el primer valo");
+		System.out.println("Imgrese el primer valor ");
 		int num1=entrada.nextInt();
-		System.out.println("Imgrese el primer valo");
+		System.out.println("Imgrese el segundo  valor");
 		int num2=entrada.nextInt();
-		MostrarCocientesEnteros.mostrarCocientes(num1, num2, 2);
+		
+		MostrarCocientesEnteros.mostrarCocientes(num1, num2,num2);
 	}
 
 }
