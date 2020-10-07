088ad4dd5356653f47b6cb5f2520af0fbf0dc0d3
Rafael Eulalio Villaneda de la torre <Rafael.rafaeua2401@outlook.com>
2020-10-06 20:28:47 -0500

    Se termino el metodo para obtener factoriales

--- src/num2_Factorial.java
+++ src/num2_Factorial.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
 
+class FactorialRecursivo{
+	public static int factorialRecursivo(int n) {
+		if(n<=1) {
+			return 1;
+		}
+		return n*factorialRecursivo(n-1);
+	}
+}
 public class num2_Factorial {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner entrada=new Scanner(System.in);
+		System.out.println("Ingresa elnumero para obtener su factorial");
+		int f=0;
+		try {
+			f=entrada.nextInt();
+			System.out.println("El factorial de :"+f+" es: "+FactorialRecursivo.factorialRecursivo(f));
+		}catch (NumberFormatException e) {
+			System.out.println("Ingresa un numero entero");
+		}
 
 	}
 
