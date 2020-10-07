a03a1a5af51bd2ea3a44ad924da07f50cac192c6
Rafael Eulalio Villaneda de la torre <Rafael.rafaeua2401@outlook.com>
2020-10-06 20:21:52 -0500

    La clase Divisores recursivos se termino

--- src/Divisores.java
+++ src/Divisores.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
 
+class DivisoresRecursivos {
+	public static void mostrarDivisores(int num,int a) {
+		if(num>1) {
+			if(num%a==0) {
+				System.out.println(a);
+				mostrarDivisores(num, a+1);
+			}else if((a+1)<=num){
+				mostrarDivisores(num, a+1);
+			}
+			if(num==1) {
+				System.out.println(num);
+			}
+		}
+	}
+}
 public class Divisores {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner entrada=new Scanner(System.in);
+		
+		System.out.println("Ingresa el numero para que te mostremos sus divisores");
+		int n=0;
+		try {
+			n=entrada.nextInt();
+		}catch (NumberFormatException e) {
+			System.out.println("No ingresaste un numero entero");
+		}
+		DivisoresRecursivos.mostrarDivisores(n,2);
 
 	}
 
